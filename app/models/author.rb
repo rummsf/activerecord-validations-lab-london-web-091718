9098d3d25bf8e0774dc1b2cceaa676f49556f082
Irum Fazal <irummf@gmail.com>
2018-10-16 16:17:01 +0000

    automatically backed up by learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,7 @@
 class Author < ActiveRecord::Base
+    validates :name, presence: true
 end
+ 
+Author.create(name: "John").valid? # => true
+Author.create(name: nil).valid? # => false
+
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true 
+  validates :content, length: { minimum: 2 }
 end
+
+Post.create(title: "happy").valid?
+Post.create(title: nil).valid?
